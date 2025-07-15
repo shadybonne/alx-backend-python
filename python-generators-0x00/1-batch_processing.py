400724c5ebf9fc1d66a1c667d265921e81bcab9b
shadybonne <shadybonne@gmail.com>
2025-07-15 17:12:11 +0000

    generator

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from seed import connect_to_prodev
+
+
+def stream_users_in_batches(batch_size):
+    pass
+    
+
+def batch_processing(batch_size):
+    pass
+
+# ["FROM user_data", "SELECT"]
+# ["25", ">"]
+# ["yield", "return"]
---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from seed import connect_to_prodev
+
+def lazy_paginate(page_size):
+    pass
+
+# SELECT FROM LIMIT WHERE
+# ["def paginate_users", "SELECT * FROM user_data LIMIT", "OFFSET"]
+# ["paginate_users(page_size, offset)"]
