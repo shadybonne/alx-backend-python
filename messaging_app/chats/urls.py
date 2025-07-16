9d8f2dd5e4036824456e06faef2ab804c5d05fa6
shadybonne <shadybonne@gmail.com>
2025-07-16 09:20:45 +0100

    urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
+
 router.register(r'conversations', ConversationViewSet)
 router.register(r'messages', MessageViewSet)
 
